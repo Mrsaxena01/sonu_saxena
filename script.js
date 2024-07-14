0243c3f402bc0b0aa6407346049389ed9abab183
Sonu Kumar <161001837+Mrsaxena01@users.noreply.github.com>
2024-07-14 21:01:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+let menu = document.querySelector("#menu");
+let slider = document.querySelector(".slider");
+let cross = document.querySelector("#cross");
+
+menu.addEventListener("click", ()=>{
+    slider.classList.toggle("view");
+    slider.style.display = 'flex';
+});
+
+cross.addEventListener("click", ()=>{
+    slider.classList.toggle("view");
+    slider.style.display = 'none';
+});
+
+
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll(".header nav a");
+
+window.onscroll = ()=>{
+    sections.forEach(sec=>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute("id");
+
+        if(top >= offset && top < offset + height  ){
+            navLinks.forEach(links =>{
+                links.classList.remove("active");
+                document.querySelector('.header nav a[href *= ' + id + ']').classList.add("active");
+            });
+        };
+    });
+};
